d58008c91c714e6f53a1ebc83c5e907daf65047e
Xihad islam <39704730+xihadulislam@users.noreply.github.com>
2023-04-12 13:24:50 +0600

    Time: 3 ms (33.80%), Space: 53 MB (15.46%) - LeetHub

---
+++ 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[][] sortTheStudents(int[][] score, int k) {
+        
+          return selectionSort(score, k);
+        
+    }
+    
+        int[][] selectionSort(int[][] score, int k) {
+
+        for (int i = 0; i < score.length - 1; i++) {
+            
+            for (int j = 0; j < score.length - i - 1; j++) {
+                if (score[j][k] < score[j + 1][k]) {
+                    int[] temp = score[j];
+                    score[j] = score[j + 1];
+                    score[j + 1] = temp;
+                }
+                
+              //  System.out.println(score[j][k] + " --  " + score[j+1][k]);
+
+            }
+            
+        }
+
+
+        return score;
+    }
+
+    
+}
